4d3d9851078fee742ad00496673cd1fef521b39e
Namit-Kanse <153580497+Namit-Kanse@users.noreply.github.com>
2025-11-12 17:59:31 +0530

    Add files via upload

--- Public/app.js
+++ Public/app.js
@@ -167,7 +167,7 @@ document.addEventListener("DOMContentLoaded", () => {
             };
 
             // Send the data to your new server
-            fetch('https://my-software-website-backend.onrender.com', {
+            fetch('https://my-software-website-backend.onrender.com/send', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
